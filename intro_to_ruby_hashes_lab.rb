9e39c0a66b0f932d098e998e412ace0906829f02
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-07 23:46:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,10 +4,7 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  second_hash = {
-    oma : "Grandma Ann" ,
-    opa : "Grandpa Charlie"
-  }
+  
 end
 
 def pioneer
